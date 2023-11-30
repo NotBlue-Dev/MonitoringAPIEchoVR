39343d62dc8d5b55798148a46ad1c59577d5d9c8
NotBlue <enzodubocage@gmail.com>
2023-11-30 17:22:58 +0100

    IPV6 to IPV4 fix

--- config/validateProviderMiddleware.js
+++ config/validateProviderMiddleware.js
@@ -2,13 +2,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 function validateProviderMiddleware(req, res, next) {
-    const clientIP = req.ip || req.connection.remoteAddress;
-
-    if (clientIP === "::1" && process.env.ENVIRONEMENT === "development") {
-        // If in development mode and IP is localhost, proceed with the middleware chain
-        return next();
+    let clientIP = req.ip || req.connection.remoteAddress;
+    if (clientIP.startsWith("::ffff:")) {
+        clientIP = clientIP.slice(7);
     }
-
     if (clientIP !== req.body.serverIp && clientIP !== req.body.ip && clientIP !== req.params.server) {
         // If IP does not match serverIP or ip from the request body, send a forbidden response
         return res.status(403).json({ message: "The IP posting on the API must match the provided server IP" });
