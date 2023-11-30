6b21500e4b823abb3db67aa1575c32165a6d2f87
NotBlue <enzodubocage@gmail.com>
2023-11-30 21:23:45 +0100

    remove key from server list

--- routes/api/listServers.js
+++ routes/api/listServers.js
@@ -1,12 +1,23 @@
-let router = require('express').Router();
-let Server = require('../../models/Server');
+const router = require('express').Router();
+const Server = require('../../models/Server');
 const limiter = require('../../config/rateLimite');
 
 router.use(limiter);
-router.get('/listServers', function(req, res, next){
-  Server.find({}, function(err, server) {
-    res.json({servers: server});
-  });
+
+router.get('/listServers', async function (req, res, next) {
+  try {
+    const servers = await Server.find({});
+
+    const serversWithoutKey = servers.map(server => {
+      const { key, ...serverWithoutKey } = server.toObject();
+      return serverWithoutKey;
+    });
+
+    res.json({ servers: serversWithoutKey });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 module.exports = router;
